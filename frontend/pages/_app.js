46060903fc6ea17e5645bcf4633aa63fb99cac58
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2025-06-14 18:52:40 +0530

    Added basic outline of the project

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({ subsets: ["latin"] });
+const geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${geistSans.className} ${geistMono.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
